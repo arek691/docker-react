ab7c5db15b9257d2149d37088a073ac154770621
arkadiusz.ziemba <arkadiusz.ziemba@gmail.com>
2023-01-05 14:12:19 +0000

    intial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Today and always
         </a>
       </header>
     </div>
